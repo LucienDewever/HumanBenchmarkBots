47f5709f9f0073f5e60d4cfdc3effcfacd93638f
Avarice <luciendewever@outlook.com>
2023-10-07 09:33:21 -0400

    basic functionality add for reaction test, hard coded values

--- python/reaction.py
+++ python/reaction.py
@@ -1,3 +1,35 @@
+from PIL import Image, ImageGrab
+import keyboard
 import pyautogui
-import time
-import win32con, win32api
+
+
+def createCoordinate():
+    placeholder =None
+
+
+
+
+xval = 980
+yval = 580
+coordinate = (980,580)
+
+
+
+
+
+def main():
+    
+    gameplay(coordinate)
+ 
+
+def gameplay(coordinate):
+    while True:
+        if keyboard.is_pressed('q'):
+            break
+        screen = ImageGrab.grab()
+        color = screen.getpixel(coordinate)
+        if color == (126, 214, 119):
+            pyautogui.click(xval,yval)
+        
+        
+main()
